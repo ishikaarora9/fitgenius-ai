28d3cee950beb38b2a33191240ededf1feba6568
Your Name <you@example.com>
2025-10-05 17:17:58 +0530

    Configure for Render deployment

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const path = require('path');
 const connectDB = require('./config/db');
 
 dotenv.config();
@@ -14,16 +15,24 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
-// Routes
+// API Routes
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/user', require('./routes/userRoutes'));
 app.use('/api/workout', require('./routes/workoutRoutes'));
 app.use('/api/diet', require('./routes/dietRoutes'));
 
-// Test route
-app.get('/', (req, res) => {
-  res.json({ message: 'FitGenius API is running!' });
-});
+// Serve static files from React app in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/dist')));
+  
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  });
+} else {
+  app.get('/', (req, res) => {
+    res.json({ message: 'FitGenius API is running!' });
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
